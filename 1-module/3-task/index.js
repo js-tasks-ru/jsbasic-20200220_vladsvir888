1f02b77a23c5759de6ea98850bad22884d4f3af5
Vlad Svirydovich <s.vlad8534@yandex.by>
2020-02-22 17:16:09 +0300

    I wrote a function that makes the first symbol uppercase

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -3,5 +3,17 @@
  * @param {string} str
  * @returns {string}
  */
+
 function ucFirst(str) {
+
+	if (!str) {
+		return str;
+	} else {
+		return str[0].toUpperCase() + str.slice(1);
+	}
+
 }
+
+alert( ucFirst('вася') );
+alert( ucFirst('в') );
+alert( ucFirst('') );
