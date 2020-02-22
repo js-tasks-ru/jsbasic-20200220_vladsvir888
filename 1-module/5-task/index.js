10fdb44d789cfea9b1cebc3a5b4fed4b1a418277
Vlad Svirydovich <s.vlad8534@yandex.by>
2020-02-23 00:35:27 +0300

    the function of truncate

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -6,14 +6,12 @@
  */
 
 function truncate(str, maxlength) {
-
-	if ( str.length > maxlength ) {
-		return str.slice(0, 19) + '…';
+	if (str.length > maxlength) {
+		return str.slice(0, maxlength - 1) + '…';
 	} else {
 		return str;
 	}
-
 }
 
-alert( `Результат: ${ truncate('Вот, что мне хотелось бы сказать на эту тему:', 20) }` );
-alert( `Результат: ${ truncate('Всем привет!', 20) }` );
+alert( `Результат: ${truncate('Вот, что мне хотелось бы сказать на эту тему:', 20)}` );
+alert( `Результат: ${truncate('Всем привет!', 20)}` );
