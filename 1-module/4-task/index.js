2dc03a7a4c335ae9d934130f553826c1227251ea
Vlad Svirydovich <s.vlad8534@yandex.by>
2020-02-22 18:07:06 +0300

    I wrote a function that checks for spam

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -3,6 +3,19 @@
  * @param {string} str base
  * @returns {boolean}
  */
+ 
 function checkSpam(str) {
 
+	let registerStr = str.toLowerCase();
+
+	if ( registerStr.includes('1xbet') || registerStr.includes('xxx') ) {
+		return true;
+	} else {
+		return false;
+	}
+
 }
+
+alert( checkSpam('1XbeT now') );
+alert( checkSpam('free xxxxx') );
+alert( checkSpam('innocent rabbit') );
