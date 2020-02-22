19da35d183a87b74f784bb117cde93be0041dbd3
Vlad Svirydovich <s.vlad8534@yandex.by>
2020-02-22 18:38:05 +0300

    I created a function that checks the length of a string

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -4,6 +4,16 @@
  * @param {number} maxlength
  * @returns {string}
  */
+
 function truncate(str, maxlength) {
 
+	if ( str.length > maxlength ) {
+		return str.slice(0, 19) + '…';
+	} else {
+		return str;
+	}
+
 }
+
+alert( `Результат: ${ truncate('Вот, что мне хотелось бы сказать на эту тему:', 20) }` );
+alert( `Результат: ${ truncate('Всем привет!', 20) }` );
